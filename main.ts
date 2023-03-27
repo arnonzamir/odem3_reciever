b0b947c7a3bfec08957ad3e85e21a48833b599f7
arnonzamir <arnonzamir@gmail.com>
2023-03-28 00:16:46 +0300

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,23 +1,32 @@
 radio.onReceivedValue(function (name, value) {
-    if (name.includes("x")) {
-        x_value = value
-        serial.writeValue("x", x_value)
-        SuperBit.Servo(SuperBit.enServo.S1, 90 + x_value)
+    if (name.includes("steering")) {
+        steering = Math.map(value, -255, 255, 0, 180)
+        serial.writeValue("steering", steering + 90)
+        SuperBit.Servo(SuperBit.enServo.S4, steering + 90)
     }
-    if (name.includes("y")) {
-        y_value = value
-        serial.writeValue("y", y_value)
+    if (name.includes("m1m3")) {
+        m1m3 = value
+        serial.writeValue("m1m3", m1m3)
         SuperBit.MotorRunDual(
         SuperBit.enMotors.M1,
-        y_value,
-        SuperBit.enMotors.M2,
-        y_value
+        m1m3,
+        SuperBit.enMotors.M3,
+        m1m3
         )
     }
+    if (name.includes("m2")) {
+        m2 = value
+        SuperBit.MotorRun(SuperBit.enMotors.M2, m2)
+    }
+    if (name.includes("m4")) {
+        m4 = value
+        SuperBit.MotorRun(SuperBit.enMotors.M4, m4)
+    }
 })
-let y_value = 0
-let x_value = 0
-radio.setGroup(1)
+let m4 = 0
+let m2 = 0
+let m1m3 = 0
+let steering = 0
 basic.showLeds(`
     # # # . .
     # . . # .
@@ -25,5 +34,12 @@ basic.showLeds(`
     # . . # .
     # . . . #
     `)
-x_value = 90
-y_value = 0
+radio.setGroup(1)
+steering = 90
+m1m3 = 0
+SuperBit.Servo(SuperBit.enServo.S4, 0)
+basic.pause(1000)
+SuperBit.Servo(SuperBit.enServo.S4, 90)
+basic.pause(1000)
+SuperBit.Servo(SuperBit.enServo.S4, 180)
+basic.pause(1000)
